e43582f974933ad216293c5cc8513097555b5134
nandologrado <nando.maciel@gmail.com>
2019-06-03 18:31:08 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/moviestore/LocadoraFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.moviestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraFilmesApplication.class, args);
+	}
+
+}
